dd2e24be2585e398c75dcdac1e3c963936c93702
chido <ogwaluchidozie@gmail.com>
2021-12-07 16:19:42 +0100

    init

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,151 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="container">
+<Header @toggle-add-task="toggleAddTask"
+ title="Task Tracker"
+ :showAddTask="showAddTask"/>
+<div v-if="showAddTask">
+<AddTask @add-task="addTask" />
+</div >
+<Tasks @toggle-reminder="toggleReminder" 
+ @delete-task="deleteTask" :tasks="tasks"/>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import Header from './components/Header'
+import Tasks from './components/Tasks'
+import AddTask from './components/AddTask'
 export default {
   name: 'App',
   components: {
-    HelloWorld
+  Header,
+  Tasks,
+  AddTask,
+
+  
+  },
+  data(){
+    return{
+      tasks:[],
+      showAddTask: true
+    }
+    
+    //if vue instance is created,
+  
+  //created() hook allows you to add code to be run
+
+  },
+  methods:{
+    toggleAddTask(){
+      this.showAddTask=!this.showAddTask
+
+    },
+    addTask(task){
+      this.tasks = [this.tasks, task]
+    },
+
+    deleteTask(id){
+      if(confirm('Are you sure?')){
+        this.tasks=this.tasks.filter((task)=> 
+      task.id !== id)
+
+      }
+      
+    },
+
+    
+    
+    toggleReminder(id) {
+      this.tasks = this.tasks.map((task) => 
+       task.id === id ? { ...task, reminder: !task.reminder
+      } : task
+    )
+    },
+    async fetchTasks(){
+      const res = await fetch('http://localhost:5000/tasks')
+      const data = await res.json()
+      return data
+    }
+  },
+  created(){
+    this.tasks=[
+      {
+        id:1,
+        text:"Doctors Appointment",
+        day: "march 1st at 10:30am",
+        reminder: true,
+      },
+      {
+        id:2,
+        text:"fellowship",
+        day: "August 1st at 6:00pm",
+        reminder: true,
+      },
+      {
+        id:3,
+        text:"Food Shoppig",
+        day: "sept 1st at 2:30pm",
+        reminder: false,
+      },
+      {
+        id:4,
+        text:"wedding",
+        day: "June 1st at 12:30pm",
+        reminder: true,
+        },
+        
+    ]
   }
+
 }
+
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+*{
+  box-sizing:border-box;
+  margin:0;
+  padding:0;
+}
+body{
+  font-family: 'poppins',Arial, Helvetica, sans-serif;
+  
+
+}
+
+.container{
+  max-width: 800px;
+  margin: 30px auto;
+  overflow: auto;
+  min-height:500px;
+  border:1px solid steelblue;
+  padding:30px;
+  border-radius:5px;
+  background-color: rgb(117, 121, 120);
+}
+.btn{
+  display:inline-block;
+  background:rgb(179, 164, 164);
+  color:#fff;
+  border:none;
+  padding:10px 20px;
+  margin:5px;
+  border-radius:5px;
+  cursor:pointer;
+  text-decoration:none;
+  font-size:15px;
+  font-family:inherit;
+
+
+}
+.btn:focus{
+  outline:none;
+}
+.btn:active{
+  transform:scale(0.98)
+}
+.btn-block{
+  display:block;
+  width:100%;
 }
 </style>
---
+++ src/components/Button.vue
@@ -0,0 +1,27 @@
+<template>
+    <button
+     @click="onClick()" :style="{ background: color}"
+    class="btn">{{ text }}</button>
+</template>
+<script>
+export default {
+    name:"button",
+    //for the colored button to be effective we add props
+    // before adding double  braces to the button class 
+    //removing the add task test
+    //for the color,we add a v-bind
+    // next is adding a method after the onclick props
+props:{
+    text: String,
+    color:String
+},
+methods: {
+    onClick(){
+        this.$emit('btn-click')
+    },
+}
+}
+</script>
+<style scoped>
+
+</style>
